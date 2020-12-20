229e411d7a48777ff17c7824bbaad5e887c38fc1
Ajetam99 <mmateja99.kg@gmail.com>
2020-12-20 04:01:08 +0100

    dodata pravila i sredjen izgled na mobilnom telefonu

--- hw-3/script.js
+++ hw-3/script.js
@@ -4,8 +4,10 @@ function submited(){
         return 0;
     }
     else{
+        var r = document.getElementById("rules");
         var x = document.getElementById("start");
         var n = document.getElementById("formName");
+        r.style.display = "block";
         n.style.display = "none";
         x.style.display = "block";
     }
@@ -16,10 +18,31 @@ function start(){
     document.getElementsByTagName("BODY")[0].style.backgroundColor = "#ff8333";
     var btn = document.getElementById("start");
     var q = document.getElementById("questions");
-
+    var r = document.getElementById("rules");
+    r.style.display = "none";
     btn.style.display = "none";
     q.style.display = "block";
+    startTimer(20)
+}
 
+function startTimer(timerLength){
+	var count = parseInt(new Date().getTime()/1000);
+	var end = parseInt(new Date().getTime()/1000) + timerLength;
+	var interval = setInterval(function(){
+        document.getElementById('timer').innerHTML=end-count;
+        if(parseInt(new Date().getTime()/1000) == count+1)
+            count++;
+        if(end-count<6){
+            p = document.getElementById("timer");
+            p.className="timer1"
+        }
+        if (count === end){
+        clearInterval(interval);
+        document.getElementById('timer').innerHTML='0';
+        p = document.getElementById("timer");
+        p.className = "timer2"
+        }
+    }, 100);
 }
 
 function darkenAnswer(x){
@@ -28,4 +51,15 @@ function darkenAnswer(x){
 
 function backAnswer(x){
     x.style.backgroundColor = "#007bff"
+}
+
+function mixQuestion(){
+    p = document.getElementById("hlp");
+    if(p.className=="help1"){
+        return 0;
+    }
+    else{
+
+        p.className = "help1"
+    }
 }
